295c6ef2ac2845ed6e9fd6ecc1aecc60354c4fed
SkyzZ-dev <jordan2officiel@gmail.com>
2022-05-01 12:12:00 -1200

    Star SkyzZ ⭐

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", function(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+
+    cursor.style.left= x + "px";
+    cursor.style.top= y + "px";
+})
